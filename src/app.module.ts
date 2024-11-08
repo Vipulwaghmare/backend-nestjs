1a6c8ba04da0cd2c00f1f8e4b791791f584993f7
vipul <vipulwaghmare222@gmail.com>
2024-11-08 18:14:39 +0530

    user services added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MessagesModule } from './messages/messages.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [MessagesModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
